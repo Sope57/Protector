e0dd067a50ee1e8000cf6b89d09706a6100062a3
ruxeom <fofojr2003@gmail.com>
2014-10-28 22:16:52 -0700

    Fused Fofo's Assets into the project
    Included test level with player and boss movement.
    Will fix assets in the future.
    Will update gitignore once I know what is crap and what isn't

---
+++ Assets/BossMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMovement : MonoBehaviour 
+{
+	private bool goingDownwards = true;
+	private float movement = 0.005f;
+	private float limit = 0.1f;
+	// Use this for initialization
+	void Start () 
+	{
+		//downwardsForce = new Vector2(0, -movementForce);
+		//upwardsForce = new Vector2(0, movementForce);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//this.transform.position = new Vector3 (0, this.transform.position.y+0.01f,0);
+		if (goingDownwards) 
+		{
+			if (this.rigidbody2D.position.y > -limit) 
+			{
+				this.transform.position = new Vector3 (0, this.transform.position.y - movement, 0);
+			}
+			else
+			{
+				goingDownwards = !goingDownwards;
+			}
+		}
+		else
+		{
+			if (this.rigidbody2D.position.y < limit) 
+			{
+				this.transform.position = new Vector3 (0, this.transform.position.y + movement, 0);
+			} 
+			else 
+			{
+				goingDownwards = !goingDownwards;
+			}
+		}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	//bool clockWise = true;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		short keyPressed = 0;
+		//Touch elTouch = Input.GetTouch (0);
+		//GameObject obj = GameObject.Find ("Player");
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			keyPressed = -1;
+		}
+		else if(Input.GetKeyDown (KeyCode.LeftArrow))
+		{
+			keyPressed = 1;
+		}
+		if(keyPressed != 0)
+		{
+			HingeJoint2D joint = this.GetComponent<HingeJoint2D>();
+			JointMotor2D motor = joint.motor;
+			motor.motorSpeed = 100 * keyPressed;
+			joint.motor = motor;
+		}
+	}
+}
